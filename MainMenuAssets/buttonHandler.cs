00a6089dfd9f18b27bfc882f30c7bc08efaeb07e
ConfusedG <drippychestercheetah02@gmail.com>
2025-04-26 17:56:50 -0400

    added story scene
    yuh

--- MainMenuAssets/buttonHandler.cs
+++ MainMenuAssets/buttonHandler.cs
@@ -4,10 +4,16 @@ using UnityEngine.SceneManagement;
 public class buttonHandler : MonoBehaviour
 {
     [SerializeField] private string controlScene = "controls";
+    [SerializeField] private string storyScene = "story";
     //add another string thing to be the play Scene
     public void loadControls()
     {
         SceneManager.LoadScene(controlScene);
+    }
+
+    public void loadStory()
+    {
+        SceneManager.LoadScene(storyScene);
     }
     public void loadGame()
     {
